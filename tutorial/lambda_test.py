2e690a89cf0f89af851b303bd82115966ab3d67c
sakhawath <sakhawath19@gmail.com>
2020-04-12 23:26:28 +0300

    lambda test file updated

--- tutorial/lambda_test.py
+++ tutorial/lambda_test.py
@@ -1,7 +1,20 @@
-a = 1
-b = 2
-x = lambda a, b: a+b
+# substituting the argument 2 for x
+print((lambda x: x)(2))
 
-print(x(a, b))
+# lambda function with one argument
+x = lambda a: a + 10
+print(x(5))
 
-val = [1, 2, 3, 4]
+# lambda function with two arguments
+y = lambda a, b: a * b
+print(y(2, 3))
+
+# lambda function with three arguments
+z = lambda a, b, c: a + b + c
+print(z(2, 3, 4))
+
+
+
+
+
+
