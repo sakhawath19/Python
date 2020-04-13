e4c4bff85c99a16cbe3c0b9fb4838aca91e43020
sakhawath <sakhawath19@gmail.com>
2020-04-13 14:20:02 +0300

    lambda_test updated

--- tutorial/lambda_test.py
+++ tutorial/lambda_test.py
@@ -1,10 +1,27 @@
+""" This script is written to understand lambda function """
+""" Filter, map and reduce function used to understand lambda function """
+""" Filter, map and reduce is highly used for lambda function """
 
-def greet(greetings, name):
-    return f"{greetings} {name}" 
+import functools
 
-print(greet("Hello", "Sakha"))
+'''
+At first step, first two elements of sequence are picked and the result is obtained.
+Next step is to apply the same function to the previously attained result and the number just succeeding the second element and the result is again stored.
+This process continues till no more elements are left in the container.
+The final returned result is returned and printed on console
+'''
+li = [1, 2, 3, 4, 5, 6, 7]
+print("The sum of the tolatl list:",\
+    functools.reduce((lambda a, b: a if a > b else b), li))
+
+# map will return value [True, False, True]
+a = [1, 2, 3, 4, 5, 6, 7]
+maped = map (lambda x: x % 2 == 0, a)  
+print(maped) # it will return object
+print(list(maped))
 
 # Filtering
+# Filter will return parameter [1, 3, 5, 7]
 li = [1, 2, 3, 4, 5, 6, 7]
 
 filtered_list= list(filter(lambda x: (x % 2 != 0), li))
