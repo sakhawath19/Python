58c086967a6487e8336f99be02acb243ab76de35
sakhawath <sakhawath19@gmail.com>
2020-04-15 17:10:55 +0300

    help_test.py

--- tutorial/help_test.py
+++ tutorial/help_test.py
@@ -1,18 +1,18 @@
 
 # Try these codes in terminal. First write python and enter to the python. Then check..
-'''
-%python 
-dir()
-dir(__builtins__)
 
-help(pow)
+dir() # returns list of the module
+
+# module object is passed as parameter
+dir(__builtins__) # returns list of the attributes
+
+help(pow) # print help of the built in power function
 
 help(hex)
 
 hex(10)
 
 import math 
-
 math.radians(10)
 
-'''
+
