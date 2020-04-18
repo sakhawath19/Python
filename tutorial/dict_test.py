d242136fcbff670130fe35e8716aa03071adba9f
sakhawath <sakhawath19@gmail.com>
2020-04-18 17:24:05 +0300

    dic_test.py

--- tutorial/dict_test.py
+++ tutorial/dict_test.py
@@ -39,3 +39,10 @@ for key, value in post.items():
 
 for value in post.values():
     print("value =", value)
+
+# merging_dicts.py
+my_first_dict = {"A": 1, "B": 2}
+my_second_dict = {"C": 3, "D": 4}
+my_merged_dict = {**my_first_dict, **my_second_dict}
+
+print(my_merged_dict)
